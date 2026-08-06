5608baa871e0e1d3526f674902eedfff6e998d00
web <146587835+ar-bh@users.noreply.github.com>
2026-08-06 16:23:37 -0400

    deleted third person controller and made first person the main one

--- player/player_3d.gd
+++ player/player_3d.gd
@@ -1,86 +1,45 @@
-class_name Player3D extends CharacterBody3D
+extends CharacterBody3D
 
+const SPEED := 5.0
+const JUMP_VELOCITY := 4.5
+var sensitivity := 0.01
+var mouse_is_playing := false
 
-@export var model: PackedScene
-
-#region camera exports
-@export_group("Camera")
-@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
-@export_range(1.0, 179.0) var camera_fov := 40.0
-#endregion
-
-#region movement exports
-@export_group("Movement")
-@export var move_speed := 8.0
-@export var acceleration := 50.0
-@export var rotation_speed := 12.0
-@export var jump_impulse := 12.0
-#endregion
-
-var _camera_input_direction := Vector2.ZERO
-var _last_movement_direction := Vector3.BACK
-var _gravity := -30.0
-
+const GRAVITY := 9.8
 
 #region node variables
-@onready var _camera_pivot: Node3D = %CameraPivot
-@onready var _camera: Camera3D = %Camera3D
-@onready var _skin: CharacterSkin3D = %CharacterSkin3D
+@onready var _head: Node3D = %Head
+@onready var _camera_3d: Camera3D = %Camera3D
 #endregion
 
-func _ready() -> void:
-	_skin.model = model
-
-
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
+	if Input.is_action_just_pressed("left_click"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if event.is_action_pressed("ui_cancel"):
+		mouse_is_playing = true
+	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-func _unhandled_input(event: InputEvent) -> void:
-	var is_camera_motion := (
-		event is InputEventMouseMotion and
-		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	)
-	if is_camera_motion:
-		_camera_input_direction = event.screen_relative * mouse_sensitivity
+		mouse_is_playing = false
 
-func _physics_process(delta: float) -> void:
-	_camera_pivot.rotation.x -= _camera_input_direction.y * delta
-	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 2.0, PI / 3.0)
-	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
-	
-	_camera_input_direction = Vector2.ZERO
-	
-	_camera.fov = camera_fov
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and mouse_is_playing:
+		_head.rotate_y(-event.relative.x * sensitivity)
+		_camera_3d.rotate_x(-event.relative.y * sensitivity)
+		_camera_3d.rotation.x = clampf(_camera_3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var forward := _camera.global_basis.z
-	var right := _camera.global_basis.x
-	
-	var move_direction := (forward * raw_input.y) + (right * raw_input.x)
-	move_direction.y = 0.0
-	move_direction = move_direction.normalized()
-	
-	var y_velocity := velocity.y
-	velocity.y = 0.0
-	
-	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
-	velocity.y = y_velocity + (_gravity * delta)
-	
-	var is_starting_jump := Input.is_action_pressed("jump") and is_on_floor()
-	if is_starting_jump:
-		velocity.y += jump_impulse
-	
-	move_and_slide()
-	
-	if move_direction.length() > 0.2:
-		_last_movement_direction = move_direction
-		_skin._play_walk_animation()
+func _physics_process(delta) -> void:
+	if not is_on_floor():
+		velocity.y -= GRAVITY * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (_head.transform.basis * Vector3(input_direction.x, 0.0, input_direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
-		_skin._play_idle_animation()
-	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	
-	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
-	
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+		velocity.z = move_toward(velocity.z, 0.0, SPEED)
+		
+	move_and_slide()
